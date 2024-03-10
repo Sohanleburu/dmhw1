b23f5c9b21cf081cc0749aa942ba1530a0e36d4e
Sohanleburu <sohan.leburu@sjsu.edu>
2024-03-09 21:54:46 -0800

    editt

--- app.py
+++ app.py
@@ -17,23 +17,14 @@ def load_data_from_bigquery(query):
     return dataframe
 
 # Query for top 10 programming tags
-query = """
-SELECT flattened_tags, COUNT(*) AS tag_count
-FROM (
-    SELECT SPLIT(tags, '|') AS tags
-    FROM `bigquery-public-data.stackoverflow.posts_questions`
-    WHERE EXTRACT(YEAR FROM creation_date) >= 2008
-), UNNEST(tags) AS flattened_tags
-GROUP BY flattened_tags
-ORDER BY tag_count DESC
-LIMIT 6
-"""
-
-# Execute the query and store the results in a pandas DataFrame.
-dataframe = client.query(query).to_dataframe()
-
-# Print the results.
-print(dataframe)
+top_tags_query = """
+SELECT flattened_tags, count(*) as tag_count from 
+(select split(tags , '|') as tags FROM `bigquery-public-data.stackoverflow.posts_questions` 
+Where EXTRACT (YEAR from creation_date) >= 2008)
+cross join unnest(tags) as flattened_tags
+group by flattened_tags
+order by tag_count desc
+limit 10
 """
 
 # Query for yearly count of questions with 'javascript' tag
